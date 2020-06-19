4809c140995a51c72d3b6ea62cdba15dca307062
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-19 17:02:58 +0800

    Configure tailwind

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -43,8 +43,7 @@ const Layout: React.FC<Props> = ({ children }) => {
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
-        }}
-      >
+        }}>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
