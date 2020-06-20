b3e81d07ef75123c2a1eb522d247e3afd52d72a9
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-20 15:34:17 +0800

    Refactor component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Header: React.FC<{}> = () => (
+  <div className="flex px-8 py-4 text-white bg-green-700">
+    <p className="text-3xl">WebChat</p>
+  </div>
+)
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import React from "react"
-import FormField from "../components/FormField"
-import Button from "../components/Button"
-import Form from "../components/Form"
-import AppLayout from "../layouts/AppLayout"
+import Header from "../components/Header"
 
 const IndexPage: React.FC<{}> = () => (
-  <div className="flex px-8 py-4 text-white bg-green-700">
-    <p className="text-3xl">
-      WebChat
-    </p>
+  <div className="">
+    <Header />
   </div>
 )
 
