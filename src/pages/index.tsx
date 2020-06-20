03811b231ee2d90c00ef6234b21699c102d9a932
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-20 16:55:15 +0800

    Add sample page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,11 +29,70 @@ const IndexPage: React.FC<{}> = () => {
 
   return (
     <div className="flex">
-      <div className="w-3/12 h-screen max-h-screen overflow-scroll">
+      <div className="w-3/12 h-screen overflow-scroll">
         <UserList userList={userList} />
       </div>
-      <div className="w-9/12 h-screen max-h-screen">
-        <Header />
+      <div className="w-9/12 h-screen">
+        <div style={{ height: "10%" }}>
+          <Header />
+        </div>
+        <div
+          className="overflow-scroll overflow-x-hidden"
+          style={{ height: "80%" }}>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+          <p>Hello</p>
+        </div>
+        <div className="p-4 bg-gray-300" style={{ height: "10%" }}>
+          <div className="flex">
+            <input
+              type="text"
+              className="px-3 py-2 mr-2 leading-tight text-gray-700 border rounded shadow appearance-none md:w-11/12 sm:w-10/12 focus:outline-none focus:shadow-outline"
+              id="username"
+              placeholder="Username"
+            />
+            <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded md:w-1/12 sm:w-2/12 hover:bg-blue-700">
+              Send
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
