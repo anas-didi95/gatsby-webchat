a8528928f3c411a1b068874a28945351e972ca8e
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-20 17:16:31 +0800

    Add sample chat interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,60 +28,61 @@ const IndexPage: React.FC<{}> = () => {
     { name: "Hello 3" },
   ]
 
+  let chatList: Types.Chat[] = [
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: false },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: false },
+    {
+      message:
+        "Hello world lksajf;saljf;aksjf;lsajf;lksajd ;ajsdf;kjas;fjas;lfdkj akajsd;fkja;f",
+      isUser: true,
+    },
+    { message: "Hello world", isUser: true },
+    {
+      message:
+        "Hello world afasfsadfsafsadfjsaf;lsajf;lsaj as;lfjsa;fjas;lfjsa;lkfjdsa;l as;kjfsa;lkfj  a;sdkjf;lsajfa;skfjsa;ldj ",
+      isUser: false,
+    },
+    { message: "Hello world", isUser: true },
+    { message: "Hello world", isUser: true },
+  ]
+
   return (
     <div className="flex">
       <div className="w-3/12 h-screen overflow-scroll">
         <UserList userList={userList} />
       </div>
       <div className="w-9/12 h-screen">
-        <div style={{ height: "10%" }}>
+        <div>
           <Header />
         </div>
         <div
-          className="overflow-scroll overflow-x-hidden"
+          className="px-8 py-4 overflow-scroll overflow-x-hidden bg-gray-300"
           style={{ height: "80%" }}>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
-          <p>Hello</p>
+          {chatList.map((chat, i) => (
+            <React.Fragment key={`chat${i}`}>
+              {chat.isUser ? (
+                <div className="flex justify-end mb-2">
+                  <p className="w-2/5 px-4 py-2 bg-green-200 rounded-md">
+                    {chat.message}
+                  </p>
+                </div>
+              ) : (
+                <div className="flex justify-start mb-2">
+                  <p className="w-2/5 px-4 py-2 bg-white rounded-md">
+                    {chat.message}
+                  </p>
+                </div>
+              )}
+            </React.Fragment>
+          ))}
         </div>
-        <div className="p-4 bg-gray-300" style={{ height: "10%" }}>
+        <div className="px-4 py-6 bg-gray-500">
           <MessageField />
         </div>
       </div>
