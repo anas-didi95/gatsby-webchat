e4569460275514f25a6609358224a0658d6be9c4
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-20 15:53:42 +0800

    Add sample component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,14 @@ import Header from "../components/Header"
 const IndexPage: React.FC<{}> = () => (
   <div className="">
     <Header />
+    <div className="flex max-h-full">
+      <div className="w-3/12 h-full h-screen max-h-screen overflow-scroll">
+        <div className="px-8 py-4 cursor-pointer hover:bg-gray-300">
+          <p className="text-lg font-semibold">Hello world</p>
+        </div>
+        <hr />
+      </div>
+    </div>
   </div>
 )
 
