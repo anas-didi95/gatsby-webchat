50d4a108d353247cf86490d215c3aa67f3f67d67
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-23 20:50:13 +0800

    Add loader gif

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react"
 
 const AppLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-row justify-center items-center h-screen bg-gray-700">
+    <div className="flex flex-row items-center justify-center h-screen bg-gray-700">
       {children}
     </div>
   )
