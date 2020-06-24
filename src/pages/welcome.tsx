f6b88b1b8c00576e75ced578283f4a3dc5345ec6
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-24 14:14:59 +0800

    Add handle button submit

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -3,18 +3,44 @@ import FormLayout from "../layouts/FormLayout"
 import Form from "../components/Form"
 import FormField from "../components/FormField"
 import Button from "../components/Button"
+import { useForm } from "react-hook-form"
+import { oc } from "ts-optchain"
+
+type TDetailForm = {
+  handleName: string
+}
 
 const WelcomePage: React.FC<{}> = () => {
+  const { register, errors, handleSubmit } = useForm<TDetailForm>()
+
+  const handler = {
+    handleButtonSubmit: handleSubmit(data => {
+      console.log("handleButtonSubmit", data)
+    }),
+  }
+
   return (
     <FormLayout>
       <p className="text-2xl font-bold">Welcome</p>
       <p className="mt-2 mb-8">
         To finalise the registration, please enter the information below.
       </p>
-      <Form title="Details Form">
-        <FormField name="handle" type="text" value="Handle name" error="" />
+      <Form title="Detail Form">
+        <FormField
+          name="handle"
+          type="text"
+          value="Handle name"
+          error={oc(errors)
+            .handleName.message("")
+            .toString()}
+          register={register({ required: "Handle name is mandatory field" })}
+        />
         <div className="flex justify-end mt-4">
-          <Button onClick={() => {}} type="primary" value="Submit" />
+          <Button
+            onClick={handler.handleButtonSubmit}
+            type="primary"
+            value="Submit"
+          />
         </div>
       </Form>
     </FormLayout>
