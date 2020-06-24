f5803c687cbd60a9c2b1dcabf6d169e683ba1c79
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-24 11:34:29 +0800

    Add loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { LoaderQuery } from "../graphqlTypes"
+import GatsbyImage, { FixedObject } from "gatsby-image"
+import { oc } from "ts-optchain"
 
-const Loader: React.FC<{}> = () => (
-  <div>hello world</div>
+const Loader: React.FC<{ data: LoaderQuery }> = ({ data }) => (
+  <div className="flex flex-col justify-center h-screen bg-gray-200">
+    <div className="flex justify-center">
+      <GatsbyImage
+        fixed={oc(data).file.childImageSharp.fixed() as FixedObject}
+      />
+    </div>
+  </div>
 )
+
+export const query = graphql`
+  query Loader {
+    file(absolutePath: { regex: "/images/loader/" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default Loader
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,7 @@
 import React, { ReactNode } from "react"
 
 const AppLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <>
-      {children}
-    </>
-  )
+  return <>{children}</>
 }
 
 export default AppLayout
