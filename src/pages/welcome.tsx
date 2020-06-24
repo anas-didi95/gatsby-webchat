425c2e249e2442a71c7e205b7a26777cf06ea7db
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-24 15:02:22 +0800

    Add handle button submit

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useContext, useState } from "react"
 import FormLayout from "../layouts/FormLayout"
 import Form from "../components/Form"
 import FormField from "../components/FormField"
 import Button from "../components/Button"
 import { useForm } from "react-hook-form"
 import { oc } from "ts-optchain"
+import LoaderContext from "../utils/contexts/LoaderContext"
+import FirebaseContext from "../utils/contexts/FirebaseContext"
 
 type TDetailForm = {
   handleName: string
@@ -12,10 +14,31 @@ type TDetailForm = {
 
 const WelcomePage: React.FC<{}> = () => {
   const { register, errors, handleSubmit } = useForm<TDetailForm>()
+  const { onLoading, offLoading } = useContext(LoaderContext)
+  const { app } = useContext(FirebaseContext)
+  const [error, setError] = useState("")
 
   const handler = {
-    handleButtonSubmit: handleSubmit(data => {
-      console.log("handleButtonSubmit", data)
+    handleButtonSubmit: handleSubmit(async ({ handleName }) => {
+      let user = app.auth().currentUser
+      console.log("handleButtonSubmit", user)
+      if (user) {
+        onLoading()
+        try {
+          await app
+            .firestore()
+            .collection("users")
+            .doc(oc(user).uid(""))
+            .set({
+              uid: oc(user).uid(""),
+              handleName: handleName,
+            })
+        } catch (e) {
+          setError(e)
+        }
+        offLoading()
+        console.log("check firebase")
+      }
     }),
   }
 
@@ -26,6 +49,7 @@ const WelcomePage: React.FC<{}> = () => {
         To finalise the registration, please enter the information below.
       </p>
       <Form title="Detail Form">
+        {error && <p className="text-sm italic text-red-500">{error}</p>}
         <FormField
           name="handle"
           type="text"
