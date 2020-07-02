bab33d58a4667f690111834314a43085e567e9b9
Anas Juwaidi <anas.didi95@gmail.com>
2020-07-02 12:14:40 +0800

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter TypeScript`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@andykenward`,
+    title: `WebChat`,
+    description: `Web-based chat application using Gatsby and Firebase`,
+    author: `@anasdidi95`,
   },
   plugins: [
     {
